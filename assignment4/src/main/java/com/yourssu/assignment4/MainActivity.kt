9a88b81cef0ce27d59321beafe773d689a6fd8be
Hanna922 <hanna.urssu@gmail.com>
2021-07-25 19:59:07 +0900

    assignment4 SharedPreferences (..?)

--- assignment4/src/main/java/com/yourssu/assignment4/MainActivity.kt
+++ assignment4/src/main/java/com/yourssu/assignment4/MainActivity.kt
@@ -18,9 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigationBar() {
-            nameButton.setOnClickListener {
-                App.prefs.nameKey = nameEditText.text.toString()
-            }
         }
     }
 
--- assignment4/src/main/java/com/yourssu/assignment4/MySharedPreferences.kt
+++ assignment4/src/main/java/com/yourssu/assignment4/MySharedPreferences.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class MySharedPreferences(context: Context) {
+    private val prefs: SharedPreferences =
+            context.getSharedPreferences("prefs_name", Context.MODE_PRIVATE)
 
-    private val prefsFilename = "prefs"
-    private val prefsKeyName = "nameKey"
-    private val prefs: SharedPreferences = context.getSharedPreferences(prefsFilename, 0)
+    fun getString(key: String, value: String): String {
+        return prefs.getString(key, value).toString()
+    }
 
-    var nameKey: String?
-        get() = prefs.getString(prefsKeyName, "")
-        set(value) = prefs.edit().putString(prefsKeyName, value).apply()
+    fun setString(key: String, str: String) {
+        prefs.edit().putString(key, str).apply()
+    }
 }
